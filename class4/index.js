0bf84690a763aa0f20aa7fafee265be3e1fadd7a
dixuanliu <dixuanliu@icloud.com>
2022-08-05 09:24:48 +0800

    2

--- class4/index.js
+++ class4/index.js
@@ -2,9 +2,17 @@ const app = Vue.createApp({
     data(){
         return{
             text:'<p>this is a <span style = "color:red;">test</span> text<p>',
-            list:[1,2,3]
+            list:[1,2,3],
+            attr:"placeholder",
+            value:"请在此输入",
+            event:"change",
         }
-    }
+    },
+    methods: {
+        handleChange(){
+            console.log("changed")
+        }
+    },
 })
 
 app.mount("#app")
